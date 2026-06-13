31bf6d5d5f1d9bc45d61d2663d23fc2d458175c2
sakibumumuni <sakibumumuni285@gmail.com>
2026-06-13 22:07:47 +0000

    add: function for anonnimity

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import pymongo
 import os
 from dotenv import load_dotenv
 import secrets
+from datetime import datetime
+
+
+
 load_dotenv() # loading environment variables
 
 app = Flask(__name__)
@@ -13,20 +17,60 @@ anonymoustokens = database.anonymoustokens
 print(client.list_database_names())
 
 # creating anonymous tokens in the database
-@app.route("/api/auth/register", methods=["GET", "POST"])
+@app.route("/anontoken", methods=["GET", "POST"])
 def create_token():
     if request.method == "POST":
-        token = secrets.token_urlsafe(10) # a 10 character string for  our token
+        token = secrets.token_urlsafe(10) # a 10 character string for  our token usable by the user anytime at login
         anonymoustokens.insert_one({"token": token})
         return jsonify({"token": token}), 201
     return jsonify({"message": "Send a POST request to create a token."}), 200
 
-# funtion that generates an anonymous name for the user
+
+"""When a user logs in, they are assigned a anonymous username and a token.
+They use this toke to login anytime they want to login and it is by tjis token
+we know their identity on the app"""
+
+@app.route("/session/<token>")
+def session_login(token):
+    user = database.find_one({"token": token})
+
+    if not user:
+        return jsonify ({"error": "Invalid Token"}), 401
+    
+    if user["is_banned"]:
+        return jsonify({"error": "Account suspended"}), 403
+    
+    new_name = generate_anon_name()
+
+    database.users.update_one(
+        {
+            "_id": user["_id"]
+        },
+        {
+            "$set": {
+                "current_name": new_name,
+                "last_seen": datetime.utcnow()
+            },
+            "$push": {"name_history": new_name}
+        }
+    )
+    return jsonify (
+        {
+            "message": "Welcome",
+            "your_name_today": new_name
+        }
+    )
+    
+
+
+# function that generates an anonymous name for the user
 def generate_anon_name ():
     adjectives = ["Kronkron", "Tumi", "Fitaa", "D)fo", "Adamfo", "Boafo", "Obiremp)n", "Ahoto"]
     nouns  = ["Okra", "Nk)sua", "Osrane", "Owia","Esono", ")soro", "Epo", "Patuo"]
     import random
     return f"Anon-{random.choice(adjectives)}{random.choce(nouns)}"
+
+
 
 
 if __name__ == "__main__":
